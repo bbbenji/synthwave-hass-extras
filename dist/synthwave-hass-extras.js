60da8caf912325c588cfecdfa70f8d7cf83c6d58
Flexage <flexage@gmail.com>
2019-11-24 04:01:21 +0000

    • Fix color of links in hass • Fix colour of links in Hacs • Add Synthwave '84 gradient at the top of the nav bar

--- dist/synthwave-hass-extras.js
+++ dist/synthwave-hass-extras.js
@@ -1,3 +1,8 @@
+const primaryColor = '#f92aad';
+const synthwaveGradient = 'linear-gradient(to right, #fc28a8, #03edf9)';
+
+let hacsInterval = null;
+
 // Method to pierce all shadowroots
 function queryShadow(root2 = document, filter = el => true, elements = []) {
   root2.querySelectorAll('*').forEach(element => {
@@ -35,15 +40,121 @@ setTimeout(function () {
           //       const icon = button_card.querySelector('#icon')
           //       icon.style.filter = 'drop-shadow( 0 0 8px #f39f0575)'
 
-          // Anchors
-          queryShadow(document, el => el.tagName === 'A')
-            .forEach(a => (a.style.color = '#f92aad'))
-          // Label classes eg. https://www.home-assistant.io/lovelace/entities/#label
-          queryShadow(document, el => el.className === 'label')
-            .forEach(a => (a.style.color = '#ffffff'))
-
+          applyStyles();
   }
   catch (e) {
     console.log(e)
   }
-}, 200)
+}, 200);
+
+// Poll for changes to window.location, so that we can apply styles to new elements when the page changes
+let lastWindowLocation = window.location.href;
+setInterval(function() {
+  // If the url has changes, apply styles
+  if(window.location.href !== lastWindowLocation) {
+    // Stop checking for changes to the hacs iframe location.href
+    clearInterval(hacsInterval);
+
+    // Try and wait for the new DOM elements to render
+    setTimeout(function() {
+      detectHacs();
+      applyStyles();
+    }, 100);
+
+    // Update the window location so that we can detect the next change
+    lastWindowLocation = window.location.href
+  }
+}, 250);
+
+// Apply custom styles
+function applyStyles() {
+  // Anchors
+  queryShadow(document, el => el.tagName === 'A')
+    .forEach(el => (el.style.color = primaryColor));
+  
+  // Label classes eg. https://www.home-assistant.io/lovelace/entities/#label
+  queryShadow(document, el => el.className === 'label')
+    .forEach(el => (el.style.color = '#ffffff'));
+
+  // Navigation Menu Icon
+  queryShadow(document, el => el.className === 'menu')
+    .forEach(el => {
+      el.style.background = synthwaveGradient;
+      el.style.backgroundSize = '100% 3px';
+      el.style.backgroundRepeat = 'no-repeat';
+      el.style.backgroundPositionY = '100%';
+      el.style.marginBottom = '5px';
+    });
+}
+
+function detectHacs() {
+  const rootEl = document.querySelector('home-assistant');
+  if(rootEl && rootEl.shadowRoot) {
+    const homeAssistantMainEl = rootEl.shadowRoot.querySelector('home-assistant-main');
+
+    if(homeAssistantMainEl && homeAssistantMainEl.shadowRoot) {
+      const appDrawerLayoutEl = homeAssistantMainEl.shadowRoot.querySelector('app-drawer-layout');
+
+      if(appDrawerLayoutEl) {
+        const haPanelIframeEl = appDrawerLayoutEl.querySelector('ha-panel-iframe');
+
+        if(haPanelIframeEl && haPanelIframeEl.shadowRoot) {
+          const hacsIframe = haPanelIframeEl.shadowRoot.querySelector('iframe');
+
+          if(hacsIframe) {
+            let lastHacsSrc = hacsIframe.contentWindow.location.href;
+
+            applyHacsStyles(hacsIframe);
+
+            // Poll for changes to the hacs iframe src
+            hacsInterval = setInterval(function() {
+              // If the url has changed, apply styles
+              if(hacsIframe.contentWindow.location.href !== lastHacsSrc) {
+                applyHacsStyles(hacsIframe);
+
+                // Update the window location so that we can detect the next change
+                lastHacsSrc = hacsIframe.contentWindow.location.href;
+              }
+            }, 250);
+          }
+        }
+      }
+    }
+  }
+}
+
+function applyHacsStyles(hacsIframe) {
+  let styles = `
+    a.actionlink {
+      color: ${primaryColor} !important;
+    }
+    
+    a.hacsbutton {
+      background-color: ${primaryColor} !important;
+    }
+    
+    .pending-upgrade {
+      color: ${primaryColor} !important;
+    }
+  `;
+
+  let styleEl = document.createElement("style"); 
+  styleEl.type = "text/css";  
+  styleEl.innerHTML = styles;
+
+  function waitForIframeAndApply(hacsIframe) {
+    // Get a handle to the iframe element
+    var iframeDoc = hacsIframe.contentDocument || hacsIframe.contentWindow.document;
+
+    // Check if loading is complete
+    if (iframeDoc.readyState == 'complete') {
+      hacsIframe.contentDocument.head.appendChild(styleEl); 
+      return;
+    } 
+
+    // If we are here, it is not loaded. Set things up so we check   the status again in 100 milliseconds
+    window.setTimeout(checkIframeLoaded, 100);
+  };
+
+  waitForIframeAndApply(hacsIframe);
+}
